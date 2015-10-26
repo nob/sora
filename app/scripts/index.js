f3d7d533eddbdf60dcb58bccd7e214b97d884c12
nob <yanbe.n@jointanet.com>
2015-10-26 12:53:28 +0900

    implementation.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,18 @@
 (function($){
-    
-    // place your awesome code here
-    
+
+    var $container = $('.gallery');
+    // init
+    $container.packery({
+      itemSelector: '.gallery > img',
+      gutter: '.gutter-sizer' ,
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+    });
+
+
+    //Modal
+    $('#myModal').on('show.bs.modal', function (e) {
+      console.log(e.relatedTarget);
+      $('img', this).attr('src', $(e.relatedTarget).attr('src'));
+    })
 })(window.jQuery);
