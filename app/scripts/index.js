a1f4126f98ba2185c906e91223442fa9925d75d5
nob <yanbe.n@jointanet.com>
2016-01-29 18:28:40 +0900

    Add intro section and intro animation.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,4 +1,4 @@
-// (function($){
+(function($){
 //
 //   function initGallery() {
 //     // Layout photo gallery items using Packery.
@@ -37,13 +37,26 @@
 //     $('.modal-body > img.photo', this).attr('src', $(e.relatedTarget).attr('src'));
 //   });
 //
-//   //Start fading Intro, now!
-//   (function () {
-//     $('#intro').delay(3000).fadeOut(1500, function(){
-//       $('.site-wrapper').show();
-//       initGallery();
-//       $('body').addClass('shown');
-//     });
-//   })();
-//
-// })(window.jQuery);
+  //Start fading Intro, now!
+  (function () {
+    $('#intro').delay(2000).fadeOut(1000, function(){
+      $('.w-section').show();
+      /**
+       * ----------------------------------------------------------------------
+       * Webflow: Interactions: Init
+       */
+      Webflow.require('ix').init([
+        {"slug":"show-interior","name":"show interior","value":{"style":{"opacity":0},"triggers":[{"type":"load","preload":true,"stepsA":[{"opacity":1,"transition":"transform 250ms ease 0ms, opacity 250ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 250ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1},{"wait":0}],"stepsB":[]}]}},
+        {"slug":"show-house","name":"show house","value":{"style":{"opacity":0},"triggers":[{"type":"load","preload":true,"stepsA":[{"wait":500},{"opacity":1,"transition":"transform 250ms ease 0ms, opacity 250ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 250ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-soda","name":"show soda","value":{"style":{"opacity":0},"triggers":[{"type":"load","preload":true,"stepsA":[{"wait":1000},{"opacity":1,"transition":"transform 250ms ease-in 0ms, opacity 250ms ease-in 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 250ms ease-in 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-notice","name":"show notice","value":{"style":{"opacity":0,"x":"-500px","y":"0px","z":"0px"},"triggers":[{"type":"scroll","offsetBot":"170px","stepsA":[{"opacity":1,"transition":"transform 500ms ease 0ms, opacity 500ms ease 0ms","x":"0px","y":"0px","z":"0px"}],"stepsB":[]}]}},
+        {"slug":"show-tea","name":"show tea","value":{"style":{"opacity":0},"triggers":[{"type":"scroll","offsetBot":"200px","stepsA":[{"opacity":1,"transition":"transform 500ms ease 0ms, opacity 500ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 500ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-open","name":"show open","value":{"style":{"opacity":0},"triggers":[{"type":"scroll","offsetBot":"300px","stepsA":[{"opacity":1,"transition":"transform 500ms ease 0ms, opacity 500ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 500ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-msg","name":"show msg","value":{"style":{"opacity":0},"triggers":[{"type":"scroll","offsetBot":"20%","stepsA":[{"opacity":1,"transition":"transform 500ms ease 0ms, opacity 500ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 500ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-house-2","name":"show house 2","value":{"style":{"opacity":0},"triggers":[{"type":"scroll","offsetBot":"40%","stepsA":[{"opacity":1,"transition":"transform 250ms ease 0ms, opacity 250ms ease 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 250ms ease 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}},
+        {"slug":"show-soda-2","name":"show soda 2","value":{"style":{"opacity":0},"triggers":[{"type":"load","preload":true,"stepsA":[{"wait":500},{"opacity":1,"transition":"transform 250ms ease-in 0ms, opacity 250ms ease-in 0ms","scaleX":1.1,"scaleY":1.1,"scaleZ":1},{"transition":"transform 250ms ease-in 0ms","scaleX":1,"scaleY":1,"scaleZ":1}],"stepsB":[]}]}}
+      ]);
+    });
+  })();
+
+})(window.jQuery);
